eea9f28748c6deccfe5f641a154823e9f76508aa
Andrei <eu.anonym93@yahoo.com>
2024-12-18 10:27:29 +0200

    Update url.blade.php
    revert back priority and freq

--- resources/views/url.blade.php
+++ resources/views/url.blade.php
@@ -10,6 +10,12 @@
 @if (! empty($tag->lastModificationDate))
     <lastmod>{{ $tag->lastModificationDate->format(DateTime::ATOM) }}</lastmod>
 @endif
+@if (! empty($tag->changeFrequency))
+     <changefreq>{{ $tag->changeFrequency }}</changefreq>
+@endif
+@if (! empty($tag->priority))
+    <priority>{{ number_format($tag->priority,1) }}</priority>
+@endif
 
     @each('sitemap::image', $tag->images, 'image')
     @each('sitemap::video', $tag->videos, 'video')
